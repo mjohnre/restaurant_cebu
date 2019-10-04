42726f849956a607545459687bf6049aa765614f
Abyanware <abyanware@abyanware.local>
2019-10-04 18:23:13 +0800

    Updates

---
+++ precache-manifest.53852e15e4b4f9acd5b0f646790694d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc52497d592b1674d0eb1a4d4c83d31",
+    "url": "/restaurant_cebu/index.html"
+  },
+  {
+    "revision": "51e1c64d36019d22ddb8",
+    "url": "/restaurant_cebu/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "68ffc12fd44a19d85dc0",
+    "url": "/restaurant_cebu/static/js/2.1c412f78.chunk.js"
+  },
+  {
+    "revision": "51e1c64d36019d22ddb8",
+    "url": "/restaurant_cebu/static/js/main.9fadec0e.chunk.js"
+  },
+  {
+    "revision": "4c1bb59302b652d96ac2",
+    "url": "/restaurant_cebu/static/js/runtime-main.8ea4badd.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/restaurant_cebu/static/media/logo.25bf045c.svg"
+  }
+]);
